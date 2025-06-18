92f3b937db7156297bb5f1f6596cb73bea70f0ab
shubham <shubhamyelekar0@gmail.com>
2025-06-18 19:36:34 +0530

    hero section

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+const Navbar = () => {
+  return (
+    <div className='font-mono uppercase flex gap-8 justify-between text-neutral-500 p-4'>
+      <Link className='text-neutral-50' href={"#"}>Home</Link>
+      <Link href={"#"}>Portfolio</Link>
+      <Link href={"#"}>Services</Link>
+      <Link href={"#"}>About</Link>
+    </div>
+  )
+}
+
+export default Navbar
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
 
 /**
  * Props for `Hero`.
@@ -15,35 +16,9 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="relative font-mono h-[80vh] overflow-hidden flex items-center justify-center"
     >
-      Placeholder component for hero (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
+      <h1 className="text-neutral-500 uppercase max-w-[600px] text-center">{slice.primary.heading}</h1>
     </section>
   );
 };
